732909751c8793cbce9b8f5ee3ab6f19cd1e6973
GoodGuyGregory <greg.witt625@gmail.com>
2025-04-29 22:06:23 -0700

    adds FRAME DURATION constant and 'b_pressed' button action to allow blocking for 0->4 iterations

--- homework/homework-1/src/main.rs
+++ homework/homework-1/src/main.rs
@@ -20,62 +20,41 @@ use rtt_target::{rprintln, rtt_init_print};
 mod life;
 use life::*;
 
-// iterate through the board until
+// Iterate through the board and randomize the values
 fn randomize_board(light_matrix: &mut [[u8; 5]; 5], hw_rng: &mut HwRng) {
-    // use into to convert the random u32 into a u128
     let mut rng = nanorand::Pcg64::new_seed(hw_rng.random_u32().into());
-    // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.flat_map
     for light in light_matrix.iter_mut().flat_map(|r| r.iter_mut()) {
         let b: bool = rng.generate();
-        match b {
-            true => *light = 1u8,
-            false => *light = 0u8,
-        }
+        *light = if b { 1u8 } else { 0u8 };
     }
 }
 
-// complements the current board being displayed.
-// making all '1' which show light turn to '0' which would be off.
+// Complements the current board being displayed
 fn complement_board(light_matrix: &mut [[u8; 5]; 5]) {
     for light in light_matrix.iter_mut().flat_map(|r| r.iter_mut()) {
-        if *light == 1u8 {
-            *light = 0u8;
-        } else {
-            *light = 1u8;
-        }
+        *light = if *light == 1u8 { 0u8 } else { 1u8 };
     }
 }
 
-// takes a mutable reference of the matrix:
-// if the method determines any lights are '1' (on)
-// it will return false and there is no need to start the timer.
-fn check_lights_out(light_matrix: &mut [[u8; 5]; 5]) -> bool {
-    for light in light_matrix.iter_mut().flat_map(|r| r.iter_mut()) {
-        if *light == 1u8 {
-            return false;
-        }
-    }
-    true
+// Checks if all lights are off
+fn check_lights_out(light_matrix: &[[u8; 5]; 5]) -> bool {
+    light_matrix.iter().flatten().all(|&light| light == 0u8)
 }
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
-    // take the board from the BSP
+    // Take the board from the BSP
     let board = Board::take().unwrap();
 
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
-
-    // import the hardware RNG from the microbit and create a new HWRng
     let mut hw_rng = HwRng::new(board.RNG);
 
-    // const for frame duration
-    const FRAME_DURATION: usize = 100;
+    const FRAME_DURATION: u16 = 100;
 
     let mut a_button = board.buttons.button_a;
-
     let mut b_button = board.buttons.button_b;
 
     let mut light_matrix = [
@@ -88,59 +67,46 @@ fn main() -> ! {
 
     randomize_board(&mut light_matrix, &mut hw_rng);
 
-    let mut b_button_ignore: bool = false;
-    let mut ignore_frame_count: usize = 0;
-
     loop {
-        // check for done. state: all display is 0:
-
         let a_pressed = a_button.is_low().unwrap();
-
         let b_pressed = b_button.is_low().unwrap();
 
         // A Button: Randomize the board display
-        //-------------------
         if a_pressed {
             rprintln!("A button pressed");
             rprintln!("Randomizing Board");
             randomize_board(&mut light_matrix, &mut hw_rng);
         }
-        // B Button:
-        // ignore the 'B' button for 0.5s post press. to prevent interupts
-        // -------------------
+        // B Button: Ignore the 'B' button for 0.5s post press
         else if b_pressed {
             rprintln!("B button pressed");
-            b_button_ignore = true;
-            rprintln!("Setting B ignore: {}", b_button_ignore)
+
+            complement_board(&mut light_matrix);
+
+            display.show(&mut timer, light_matrix, FRAME_DURATION.into());
+
+            for _ in 0..4 {
+                
+                rprintln!("Step through Complemented Life");
+                life(&mut light_matrix);
+
+                complement_board(&mut light_matrix);
+
+                display.show(&mut timer, light_matrix, FRAME_DURATION.into());
+
+            }
         }
-        // checks if all of the cells on the display matrix are off.
-        else if check_lights_out(&mut light_matrix) {
-            // wait 0.5 seconds then re randomize.
+        // Check if all lights are out
+        else if check_lights_out(&light_matrix) {
             rprintln!("Waiting for Input");
-            timer.delay_ms((5 * FRAME_DURATION).try_into().unwrap());
+            timer.delay_ms(FRAME_DURATION.into());
             randomize_board(&mut light_matrix, &mut hw_rng);
         }
 
-        // DEFAULT: Otherwise normal life steps are taken:
-
+        // Step through life
         rprintln!("Stepping Through Life");
         life(&mut light_matrix);
 
-        if b_button_ignore {
-            if ignore_frame_count < 4 {
-                ignore_frame_count += 1;
-                rprintln!("Checking Ignore Frame Count: {}", ignore_frame_count);
-                complement_board(&mut light_matrix);
-            } else {
-                rprintln!("Switching off B Ignore:");
-                b_button_ignore = false;
-                rprintln!("Resetting Ignore Frame Count:");
-                ignore_frame_count = 0;
-            }
-        }
-
-        display.show(&mut timer, light_matrix, FRAME_DURATION.try_into().unwrap());
-        // clear the display again
-        display.clear();
+        display.show(&mut timer, light_matrix, FRAME_DURATION.into());
     }
 }
